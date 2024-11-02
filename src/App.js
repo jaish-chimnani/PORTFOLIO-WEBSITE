317f355ed63dc7cd3788f63491fb2edcb3dfbd3b
Jaish Chimnani <jaish.chimnani@geminisolutions.com>
2024-11-02 15:58:44 +0530

    fixed site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
-    
-      <Main/>
+    THIS WEBSITE IS UNDER MAINTAINANCE
+      {/* <Main/> */}
     </div>
   );
 }
